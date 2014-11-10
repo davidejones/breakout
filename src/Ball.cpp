c8191f9d1187f4563d01f189975a62d49c019d4c
David Jones <david@davidejones.com>
2014-11-09 23:44:47 -0800

    experiment

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -10,7 +10,7 @@ Ball::Ball()
 	direction.Y = 1;
 	position.X = 600 * 0.5;
 	position.Y = 600 * 0.5;
-	speed = 300;
+	speed = 100.0f; // pixels per second
     bounds.minX = position.X - 10;
 	bounds.maxX = position.X + 10;
 	bounds.minY = position.Y - 10;
@@ -21,54 +21,58 @@ void Ball::render()
 {
 	glPushMatrix();
 	glTranslated(position.X, position.Y, 0);
-	filledCircle();
+	//filledCircle();
+	drawCircle(-1, 0, 10, 20);
 	glPopMatrix();
 }
 
 void Ball::update(float dt)
 {
-	position.X = position.X + direction.X * (speed * dt);
-	position.Y = position.Y + direction.Y * (speed * dt);
-
-	//if ball hits right side
-	if(position.X >= 600)
-	{
-		direction.X *= -1;
-	}
-
-	//if ball hits left side
-	if(position.X <= 0)
-	{
-		direction.X *= -1;
-	}
-
-	//if ball hits the bottom
-	if(position.Y >= 600)
-	{
-		cout << "Life Lost.." << endl;
-
-		//reset variables and ballpos
-		speed = 250;
-		position.X = 600 * 0.5;
-    	position.Y = 600 * 0.5;
-	}
-
-	//if ball hits the top
-	if(position.Y <= 0)
-	{
-		direction.Y *= -1;
-	}
+	position.X += direction.X * speed * dt;
+	position.Y += direction.Y * speed * dt;
 
 	//update bounding box
 	bounds.minX = position.X - 10;
 	bounds.maxX = position.X + 10;
 	bounds.minY = position.Y - 10;
 	bounds.maxY = position.Y + 10;
+
+	//if ball hits right side
+	if(bounds.maxX >= 600)
+	{
+		direction.X *= -1;
+	}
+
+	//if ball hits left side
+	if(bounds.minX <= 0)
+	{
+		direction.X *= -1;
+	}
+
+	//if ball hits the bottom
+	if(bounds.maxY >= 600)
+	{
+		cout << "Life Lost.." << endl;
+
+		//reset variables and ballpos
+		speed = 100.0f;
+		direction.X = 1;
+		direction.Y = 1;
+		position.X = 600 * 0.5;
+    	position.Y = 600 * 0.5;
+	}
+
+	//if ball hits the top
+	if(bounds.minY <= 0)
+	{
+		direction.Y *= -1;
+	}
 }
 
 void Ball::drawCircle(float cx, float cy, float r, int num_segments)
 {
-    glBegin(GL_LINE_LOOP);
+    //glBegin(GL_LINE_LOOP);
+    glBegin(GL_TRIANGLE_FAN);
     for(int ii = 0; ii < num_segments; ii++)
     {
         float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
@@ -76,35 +80,12 @@ void Ball::drawCircle(float cx, float cy, float r, int num_segments)
         float x = r * cosf(theta);//calculate the x component
         float y = r * sinf(theta);//calculate the y component
 
-        glColor3f(1.0f, 0.0f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
         glVertex2f(x + cx, y + cy);//output vertex
 
     }
     glEnd();
 }
-
-void Ball::filledCircle()
-{
-	float x1,y1,x2,y2;
-	float angle;
-	double radius=10;
-
-	x1=0,y1=0;
-	glColor3f(0.0,0.0,1.0);
-
-	glBegin(GL_TRIANGLE_FAN);
-	glVertex2f(x1,y1);
-
-	for (angle=1.0f;angle<361.0f;angle+=0.2)
-	{
-		x2 = x1+sin(angle)*radius;
-		y2 = y1+cos(angle)*radius;
-		glVertex2f(x2,y2);
-	}
-
-	glEnd();
-} 
-
 
 void Ball::doPaddleCollision(BoundingBox paddlebounds)
 {
@@ -133,33 +114,29 @@ void Ball::doBrickCollision(Brick *brick)
 
 	//cout << direction.X << " " << direction.Y << endl;
 	
-	if((bounds.maxX-5) <= brick->bounds->minX) {
-		//cout << "left" << endl;
-		position.X = brick->bounds->minX;
-		direction.X = -direction.X;
+	if(bounds.maxX < (brick->bounds->minX + 40) && bounds.maxX >= brick->bounds->minX) {
+		cout << "left" << endl;
+		position.X = bounds.minX-10;
+		//direction.X = -direction.X;
 	}
 
-	if((bounds.minX+5) >= (brick->bounds->maxX)) {
-		//cout << "right" << endl;
-		position.X = brick->bounds->maxX;
-		direction.X = -direction.X;
+	if(bounds.minX > (brick->bounds->maxX - 40) && bounds.minX <= brick->bounds->maxX) {
+		cout << "right" << endl;
+		position.X = bounds.maxX-10;
+		//direction.X = -direction.X;
 	}
 
-	if((bounds.minY-5) <= brick->bounds->minY) {
-		//cout << "top" << endl;
-		position.Y = brick->bounds->minY;
+	//if less than center point and less than the miny 
+	if(bounds.minY < (brick->bounds->minY + 10) && bounds.minY >= brick->bounds->minY) {
+		cout << "top" << endl;
+		position.Y = brick->bounds->minY-5;
 		direction.Y = -direction.Y;
 	}
 
-	if((bounds.minY+5) >= (brick->bounds->maxY)) {
-		//cout << "bottom" << endl;
-		position.Y = brick->bounds->maxY;
+	//if greater than center point but less than maxy - brick size 80/20 w/h
+	if(bounds.minY > (brick->bounds->maxY - 10) && bounds.minY <= brick->bounds->maxY) {
+		cout << "bottom" << endl;
+		position.Y = brick->bounds->maxY+5;
 		direction.Y = -direction.Y;
 	}
-
-	//update bounding box
-	bounds.minX = position.X - 10;
-	bounds.maxX = position.X + 10;
-	bounds.minY = position.Y - 10;
-	bounds.maxY = position.Y + 10;
 }
